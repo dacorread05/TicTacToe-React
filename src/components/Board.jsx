3a513ef843f77b4bd26382c35c91095464497471
Diego Correa Diaz <dacorrea91@ucatolica.edu.co>
2020-10-27 17:23:57 -0500

    Finalizada la primera version

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,42 +1,37 @@
 import React, {Component} from 'react';
 import Square from './Square';
+import calculateWinner from '../index';
 
 class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        };
-    }
+
     renderSquare(i) {
-      return <Square  value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      return <Square  value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
       />;
     }
   
     render() {
-      const status = 'Next player: X';
-  
       return (
-        <div>
-          <div className="status">{status}</div>
+        <div className="board">
           <div className="board-row">
-            {this.renderSquare()}
-            {this.renderSquare()}
-            {this.renderSquare()}
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare()}
-            {this.renderSquare()}
-            {this.renderSquare()}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare()}
-            {this.renderSquare()}
-            {this.renderSquare()}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
           </div>
         </div>
       );
     }
 }
+
+
 export default Board;
